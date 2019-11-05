0218bc6c469633e0015df8f6ecb1d00b84bf35d4
João Melo <jopcmelo@gmail.com>
2019-11-05 16:13:40 -0300

    Update reducer.js

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,6 +1,6 @@
 import produce from 'immer';
 
-export default function user(state = {}, action) {
+export default function User(state = {}, action) {
   switch (action.type) {
     case '@user/ADD_SUCCESS':
       return produce(state, draft => {
